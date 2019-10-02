d3951018f0e1ab06dbda9ef37919d71dd158f9e0
BuildTools <unconfigured@null.spigotmc.org>
2019-10-02 23:44:48 +0800

    add

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+/**
+ * Writing data for hacking stem experiments
+ */
+//% weight=94 color=#EC7505 icon="\uf1b3"
+namespace STEM {
+    /**
+     * Checks if the goal is reached by looking for the Gold Block direction down to agent
+     */
+    //% blockId=send_message block="send message %message"
+    //% weight=30
+    export function sendMessage(message: string) {
+        // BrowserUtils.loadAjaxAsync("http://www.baidu.com");
+    }
+}
